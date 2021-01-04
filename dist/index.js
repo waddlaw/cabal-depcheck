9fa6ae92dfdea4b8107eb406a7b89feea52a5e38
waddlaw <ingronze@gmail.com>
2021-01-04 17:10:43 +0900

    wip

--- dist/index.js
+++ dist/index.js
@@ -56,10 +56,10 @@ async function run() {
     // Run cabal outdated
 
     // await exec.exec(cabal_install_cached_path, ['--version']);
-    await exec.exec(
-        cabal_install_cached_path,
-        ['outdated']
-    );
+    // await exec.exec(
+    //     cabal_install_cached_path,
+    //     ['outdated']
+    // );
 
   } catch (error) {
     core.setFailed(`Action failed with error ${error}`);
