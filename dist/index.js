0075750fd9a9870289c86361a69ee3f29f58be56
waddlaw <ingronze@gmail.com>
2021-01-04 17:15:52 +0900

    wip

--- dist/index.js
+++ dist/index.js
@@ -55,7 +55,7 @@ async function run() {
 
     // Run cabal outdated
 
-    await exec.exec('cat', cabal_install_cached_path);
+    await exec.exec('ls', ['-l', cabal_install_cached_path]);
 
     // await exec.exec(cabal_install_cached_path, ['--version']);
     // await exec.exec(
